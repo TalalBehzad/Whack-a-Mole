44663fae31edf059bb9156b25adfd38572b20df1
TalalBehzad <talalhaniali@gmail.com>
2025-05-24 01:46:13 +0300

    Finalized my work, although I'm running through a problem with deploying my game

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-let currentMole;//This represents the current moletile
-let currentbomb;//This represents the bomb tile
-let score = 0 //Represents the score 
-let gameOver = false; //Represents the gameover
+let currentMole;
+let currentbomb;
+let score = 0 
+let gameOver = false;
 let moleInterval;
 let bombInterval;
 
 window.onload = function () {
  setGame();
-moleInterval = setInterval(setMole, 1000); //We will use 1000 miliseconds which equals to 1 seconds for everytime the Mole appears on the screen.
+moleInterval = setInterval(setMole, 1000); 
 bombInterval = setInterval(setbomb, 2000);
 }
 
 
 
-//Since there are nine tiles we will create an IF statment with the 9. Which means that if i is less then 9 it will go from 0 to 8.
 function setGame() {
 for (let i = 0; i < 9; i++ ) {
-      // We will use the <div> id= '0-8' </div> to assign each tile an ID
    let tile = document.createElement('div');
    tile.id = i.toString();
    tile.addEventListener('click', selectTile);
    document.getElementById('board').appendChild(tile);
 }
-//We will use 1000 miliseconds which equals to 1 seconds for everytime the Mole appears on the screen.
- //We will use 2000 miliseconds which equals to 2 seconds for everytime the Bomb appears on the screen.
 }
 
 function init(){
@@ -46,13 +42,11 @@ function setMole() {
    if (gameOver) {
       return;
    }
-   //By adding a gameover if for both the set mole function and the bomb function, this makes it so that when the game is over the mole and bomb stop moving all togather.
    if (currentMole) {
       currentMole.innerHTML = '';
    }
    let mole = document.createElement('img');
    mole.src = './images/Mole.png';
-   //Now we will need to input the mole on a random tile. This means that I will have to create a variable for number and make the attribute to a randomtile.
    let num = getRandomTile()
    if (currentbomb && currentbomb.id === num) {
       return;
@@ -65,7 +59,6 @@ function getRandomTile() {
    let num = Math.floor(Math.random() * 9);
    return num.toString();
 }
-//Math.random will give me a number between 0 and one and if I multiply it to 9, The range will become from 0-9, then we round it down with math.floor we will get numbers from 0-8. After that use string for the id.
 
 
 function setbomb() {
@@ -79,16 +72,13 @@ function setbomb() {
    let bomb = document.createElement('img');
    bomb.src = './images/Bomb1.png';
 
-   //Now with the bomb we will use a random tile to decide where the bomb is placed. 
 
    let num = getRandomTile();
    if (currentMole && currentMole.id === num) {
       return;
    }
-   //By creating a if for both of the mole and bomb, this helps us so that the bomb and mole are showen accordingly in the holes. 
    currentbomb = document.getElementById(num)
    currentbomb.appendChild(bomb); 
-   //We are doing the same steps exactly as the previous mole tile
 }
 
 
@@ -102,18 +92,15 @@ function selectTile() {
       score += 10;
       document.getElementById('score').innerText = score.toString(); 
       currentMole.querySelector('img').src = './images/MoleHit.png';
-      //This makes it so that the score gets updated each time the mole gets clicked on 
    }
    else if (this === currentbomb) {
       document.getElementById('score').innerText = 'GAME OVER: ' + score.toString();
       gameOver = true;
-      //This creates for us a gameover once the bomb has been clicked accidently. 
    }
 }
 
-//progress bar countdown timer
 let progressBarElement = document.querySelector('#progress-bar')
-let gameDuration = 30; //represents the seconds for the bar
+let gameDuration = 30; 
 let barNum = gameDuration*1000;
 const barDen = gameDuration*1000;
 let barInterval = setInterval(progressbarframe, 10);
@@ -140,7 +127,6 @@ function progressbarframe() {
 }
 
 
-//win/lose logic 
 function endgame(){
    clearInterval(barInterval);
    gameOver = true;
@@ -155,25 +141,47 @@ function endgame(){
 const resetBtn = document.getElementById('reset'); 
 resetBtn.addEventListener('click', init);
 
+//Code Graveyard comments 
+//This represents the current moletile
 
-// function render() {
-// gameOver();
-// }
-// if (score >= 300) {
-//    gameOver('You Win!');
-// } else {
-//    gameOver('Times up, you lose!');
-// }
+//This represents the bomb tile
 
-// function init(){
-// currentMole;
-// currentbomb;
-// score = 0 
-// gameOver = false; 
-// progressBarElement = document.querySelector('#progress-bar')
-// gameDuration = 30; //represents the seconds for the bar
-// barNum = gameDuration*1000;
-// barDen = gameDuration*1000;
-// barInterval = setInterval(progressbarframe, 10);
+//Represents the score
 
-// }
+//Represents the gameover
+
+//We will use 1000 miliseconds which equals to 1 seconds for everytime the Mole appears on the screen.
+
+//Since there are nine tiles we will create an for loop statment with the 9. Which means that if i is less then 9 it will go from 0 to 8.
+
+// We will use the <div> id= '0-8' </div> to assign each tile an ID
+
+//We will use 1000 miliseconds which equals to 1 seconds for everytime the Mole appears on the screen.
+
+//We will use 2000 miliseconds which equals to 2 seconds for everytime the Bomb appears on the screen.
+
+//By adding a gameover if for both the set mole function and the bomb function, this makes it so that when the game is over the mole and bomb stop moving all togather.
+
+//Now we will need to input the mole on a random tile. This means that I will have to create a variable for number and make the attribute to a randomtile.
+
+//Math.random will give me a number between 0 and one and if I multiply it to 9, The range will become from 0-9, then we round it down with math.floor we will get numbers from 0-8. After that use string for the id.
+
+//Now with the bomb we will use a random tile to decide where the bomb is placed.
+
+//By creating a if for both of the mole and bomb, this helps us so that the bomb and mole are showen accordingly in the holes.
+
+//We are doing the same steps exactly as the previous mole tile
+
+//This makes it so that the score gets updated each time the mole gets clicked on
+
+//This creates for us a gameover once the bomb has been clicked accidently.
+
+//progress bar countdown timer
+
+//represents the seconds for the bar
+
+//win/lose logic
+
+//reset button 
+
+
--- readme.md
+++ readme.md
@@ -1,5 +1,34 @@
 # Whack a Mole 🦦
-![Mole Charcter](Mole.png)
 
-*Hello welcome to Whack a Mole. The purpose of this game is to achieve the highest points of hit on the Mole.*
+![alt text](<Screenshot 2025-05-23 213052-1.png>)
 
+**Welcome to Whack a Mole. In this fast-paced web game players click on randomly appearing moles on a 3x3 grid to test their reflexes. You get a timer set at 30 seconds, the objective is to score as many points as you can. You get points when you click on moles, but watch out, because clicking on a bomb immediately ends the game!**
+
+## Features of the game 
+- ***Random mole and bomb appearances***
+- ***A scoring system with win/loss messages***
+
+- ***A reset button to restart the game anytime***
+- ***A countdown progress bar***
+
+
+``Tip: For extra points, double click the Mole as soon as he appears in the Hole, but be careful because sometimes the same Hole get's replaced with a bomb``
+
+## Attributions and Technologies
+
+- [Sprites/images](<../../../../OneDrive/Desktop/Whack A Mole>) 
+
+![alt text](Mole.png)
+![alt text](image.png)
+![alt text](images/HoleBack.png)
+![alt text](images/Bomb1.png)
+
+- ``javascript, index.html, style.css, readme.md``
+
+## Planned future enhancement
+- **Adding a difficulty to the game (such as making the mole faster and more bombs being in the tiles)**
+
+- **Time boost or freeze power-ups**
+- **Animated Mole(eg, popping up/down,exploding bomb)**
+- **adding sound effects to the game**
+- **High score Tracking**
